48abaf832fd96a6ed31a379ec00e9f4b57b12c2c
jiwan <jiwankim@kyolimsoft.co.kr>
2020-05-26 17:57:52 +0900

    gitignore

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.web;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -23,4 +23,17 @@ public class HelloControllerTest {
         String hello = "hello";
         mvc.perform(MockMvcRequestBuilders.get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
     }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello/dto").param("name",name).param("amount",String.valueOf(amount))).andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", Matchers.is(name)))
+                .andExpect(jsonPath("$.amount",Matchers.is(amount)));
+    }
+
+
+
 }
